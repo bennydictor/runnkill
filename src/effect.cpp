5a2e42a8a7653718289219a7ae9f0f6db882a9d6
Z <s16b2_aleykin@179.ru>
2015-08-24 20:11:41 +0000

    one small bad symbol

--- src/effect.cpp
+++ src/effect.cpp
@@ -1,8 +1,8 @@
-#include <effects.h>
+#include <effect.h>
 #include <mod_type.h>
 using namespace std;
 
-mod_t effects::tic(float t) {
+mod_t effect::tic(float t) {
     t = min(t, time);
     mod_t ret;
     ret = mods * t;
