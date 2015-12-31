f715b10bac84b547d240ceef0065e1784791ebf5
Z <s16b2_aleykin@179.ru>
2015-12-31 17:41:56 +0000

    changed all

--- src/include/net/socket.h
+++
@@ -1,9 +0,0 @@
-#ifndef NET_SOCKET_H
-#define NET_SOCKET_H
-
-#include <stdint.h>
-
-
-int make_local_socket(uint16_t port);
-
-#endif // NET_SOCKET_H
--- src/include/util/file.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTIL_FILE_H
-#define UTIL_FILE_H
-
-char *read_file(const char *filename);
-
-#endif // UTIL_FILE_H
